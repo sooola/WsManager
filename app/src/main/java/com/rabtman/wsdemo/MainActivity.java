0d1c22c0a7bf872eec78119998ef443667c10309
wei <329377653@qq.com>
2020-05-19 16:55:55 +0800

    更新到androidx，修复断网不能短线重连问题

--- app/src/main/java/com/rabtman/wsdemo/MainActivity.java
+++ app/src/main/java/com/rabtman/wsdemo/MainActivity.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.view.ScrollingView;
-import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.Spanned;
 import android.text.TextUtils;
@@ -22,9 +19,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.ScrollingView;
+
 import com.rabtman.wsmanager.WsManager;
 import com.rabtman.wsmanager.listener.WsStatusListener;
+
 import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Response;
 import okio.ByteString;
